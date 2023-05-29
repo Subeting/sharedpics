0953e26ad7605bbe4133c65bf56023d8ab8b5fc6
Subeting <68261543+Subeting@users.noreply.github.com>
2023-05-29 15:26:58 +0800

    Update example_rand_gw.sh

--- vps/example_rand_gw.sh
+++ vps/example_rand_gw.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ###Please defind your gateway address here###
 target_gw=2222:2222::2222
-###Please defind your main interface here###
-net_iface=ens18
+###Please defind your main interface here ###
+#net_iface=ens18#Automatically determine your MAIN IFACE
+net_iface=$(ip r g 254.254.254.254|grep via|head -n 1|awk -v FS='dev ' '{print $2}'|cut -d ' ' -f 1)
 
 echo "net.ipv4.ip_local_port_range = 32768 64999" >> /etc/sysctl.conf
 ip r a 2602:fbda:730:aaaa::/48 dev $net_iface
